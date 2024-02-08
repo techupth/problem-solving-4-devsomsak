d051ee9f5bf7695d5cd3a4a60beada0f18820e7c
devsomsak <devrleang@gmail.com>
2024-02-08 16:30:54 +0700

    fix: solve problem solving-4

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,30 @@
-function pivotIndex(nums) {}
+function sumArray(nums) {
+  let total = 0;
+  for (let i = 0; i < nums.length; i++) {
+    total += nums[i];
+  }
+  return total;
+}
+//Mynote
+//    function sumArray(nums) {
+//     return nums.reduce((acc, curr) => acc + curr, 0);
+// }
+
+function pivotIndex(nums) {
+  let total = sumArray(nums);
+  let leftSum = 0;
+
+  for (let i = 0; i < 4; i++) {
+    if (leftSum === total - leftSum - nums[i]) {
+      console.log(leftSum);
+      return i;
+    }
+    leftSum += nums[i];
+    console.log(leftSum);
+  }
+
+  return -1;
+}
+
+let nums = [1, 7, 3, 6, 5, 6];
+console.log(pivotIndex(nums)); // Output: 3
--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,15 @@
-function defangIPaddr(nums) {}
+function defangIPaddr(nums) {
+  let defangedAddress = "";
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === ".") {
+      defangedAddress += "[.]";
+    } else {
+      defangedAddress += nums[i];
+    }
+  }
+  return defangedAddress;
+}
+
+let ipAddress = "1.1.1.1";
+let defangedIP = defangIPaddr(ipAddress);
+console.log(defangedIP); // Output: "1[.]1[.]1[.]1"
